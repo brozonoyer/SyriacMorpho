2ab7a95dd9ed98f09c2475cf90a3e726d82a0c11
benjaminrozonoyer <brozonoyer@brandeis.edu>
2019-03-24 19:27:38 -0400

    update

--- Transliterator.py
+++ Transliterator.py
@@ -1,18 +1,45 @@
-import os, json
+import os, json, unicodedata
 from collections import defaultdict
 
 class Transliterator():
 
     def __init__(self):
-        dict = json.loads('syriac_translit.json')
-        print(dict)
+        self.together = {"\u0741","\u0742", "\u073C","\u073F", "\u0323"}
+        with open('syriac_transl.json', 'r') as f:
+            self.dict = json.load(f)
 
-    def syriac2latin(self):
-        pass
+    def transliterate(self, syr):
+        '''
+        :param syr: word in Syriac
+        :return: transliterated
+        '''
+        result = []
+        for i, letter in enumerate(syr):
+            print("––––––––––––––––––––––––––––––––––")
+            print(i, 'U+%04x' % ord(letter), result)
+            if syr[i] in self.together:
+                result[-1] = self.dict[syr[i-1:i+1]]
+            else:
+                result.append(self.dict[letter])
+            print(i, 'U+%04x' % ord(letter), result)
 
-    def latin2syriac(self):
-        pass
+        return ''.join(result)
+
 
 if __name__ == '__main__':
     T = Transliterator()
-    #print('ܕ' == '\u0715')
+    #simplified = T.transliterate("ܕܚܸܫܘܿܟ̣ܝܢ")
+    #print(simplified)
+    #transliterated = T.transliterate("ܘܲܢܓܲܕܼ ܐܲܝܬ݁ܝܼ ܡܓ̣ܘܼ̈ܫܹܐ")
+    transliterated = T.transliterate("ܕܒܲܣܢܝܼܩܘܼܬܹܗ")
+    #transliterated = T.transliterate("ܕܲܦܬܲܟ̣ܪܘܼܬܼܵܐ")
+    print(transliterated)
+'''
+    ܕ  d
+    ܚ  hdot
+    ܸ   e
+    ܫ shin
+    ܘ waw
+    ܿ waw-over
+    ܟ̣ܝܼܢ
+'''

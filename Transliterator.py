59a2a3c824109e35448cadc4cdaf600889793f24
benjaminrozonoyer <brozonoyer@brandeis.edu>
2019-04-06 15:54:48 -0400

    update

--- Transliterator.py
+++ Transliterator.py
@@ -5,6 +5,7 @@ class Transliterator():
 
     def __init__(self):
         self.together = {"\u0741","\u0742", "\u073C","\u073F", "\u0323"}
+        self.linea_occultans = "\u0304"
         with open('syriac_transl.json', 'r') as f:
             self.dict = json.load(f)
 
@@ -16,9 +17,12 @@ class Transliterator():
         result = []
         for i, letter in enumerate(syr):
             print("––––––––––––––––––––––––––––––––––")
-            print(i, 'U+%04x' % ord(letter), result)
+            print(i, 'U+%04x' % ord(letter), letter, result)
+            #print("occultans", ord(self.linea_occultans))
             if syr[i] in self.together:
                 result[-1] = self.dict[syr[i-1:i+1]]
+            elif syr[i] == self.linea_occultans:
+                continue
             else:
                 result.append(self.dict[letter])
             print(i, 'U+%04x' % ord(letter), result)
@@ -31,8 +35,10 @@ if __name__ == '__main__':
     #simplified = T.transliterate("ܕܚܸܫܘܿܟ̣ܝܢ")
     #print(simplified)
     #transliterated = T.transliterate("ܘܲܢܓܲܕܼ ܐܲܝܬ݁ܝܼ ܡܓ̣ܘܼ̈ܫܹܐ")
-    transliterated = T.transliterate("ܕܒܲܣܢܝܼܩܘܼܬܹܗ")
+    #transliterated = T.transliterate("ܕܒܲܣܢܝܼܩܘܼܬܹܗ")
+    #transliterated = T.transliterate("ܒܚܘܼܒܵܐ")
     #transliterated = T.transliterate("ܕܲܦܬܲܟ̣ܪܘܼܬܼܵܐ")
+    transliterated = T.transliterate('')
     print(transliterated)
 '''
     ܕ  d

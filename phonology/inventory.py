7b8ee4d70956154e7788ed7a0badedbb3316630b
benjaminrozonoyer <brozonoyer@brandeis.edu>
2019-05-21 21:20:04 -0700

    update

--- phonology/inventory.py
+++ phonology/inventory.py
@@ -1,5 +1,30 @@
 
 class Inventory():
 
-    def __init__(self, file_path):
-        pass
+    def __init__(self, inventory_path='./inventory'):
+        self.inventory = self.load_inventory(inventory_path=inventory_path)
+
+    def load_inventory(self, inventory_path='./inventory'):
+        '''
+        :param path:
+        :return:
+        '''
+
+        with open(inventory_path, 'r') as f:
+            segments = [s.strip().split() for s in f.readlines()]
+
+        feature_types = segments[0][1:]
+        segment_list = segments[1:]
+
+        inventory = {}
+        for s in segment_list:
+            segment = s[0]
+            segment_dict = {}
+            for i, value in enumerate(s[1:]):
+                segment_dict[feature_types[i]] = value
+            inventory[segment] = segment_dict
+
+        return inventory
+
+if __name__ == '__main__':
+    I = Inventory()
